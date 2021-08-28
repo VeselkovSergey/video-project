5cccfcb33ef8d6c05bf600215e000dfb9020406a
Sergey <s-vesel94@ya.ru>
2021-08-29 01:36:22 +0300

    -

--- app/Http/Controllers/Files.php
+++ app/Http/Controllers/Files.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\FileHelpers;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Files as FilesDB;
@@ -43,12 +44,44 @@ class Files
     public function GetFileByHash(Request $request) {
         $file = FilesDB::where('hash_name', $request->hashFile)->first();
         if ($file) {
-            $filePath = Storage::disk($file->disk)->get($file->path . '/' . $file->hash_name);
-            return response($filePath)->withHeaders([
-                'Content-type' => $file->type,
-                'Content-Length' => $file->size,
-                'Accept-Ranges' => 'bytes'
-            ]);
+            if (in_array($file->type, ['video/mp4', 'video/x-m4v', 'video/*', 'video/webm'])) {
+                $path = './' . $file->hash_name;
+                $stream = fopen($path, "r");
+                $response_code = 200;
+                $headers = array("Content-type" => $file->type);
+
+                // Check for request for part of the stream
+                $size = $file->size;
+                $range = $request->header('Range');
+                if($range != null) {
+                    $eqPos = strpos($range, "=");
+                    $toPos = strpos($range, "-");
+                    $unit = substr($range, 0, $eqPos);
+                    $start = intval(substr($range, $eqPos+1, $toPos));
+                    $success = fseek($stream, $start);
+                    if($success == 0) {
+                        $size = $file->size - $start;
+                        $response_code = 206;
+                        $headers["Accept-Ranges"] = $unit;
+                        $headers["Content-Range"] = $unit . " " . $start . "-" . ($file->size-1) . "/" . $file->size;
+                    }
+                }
+
+                $headers["Content-Length"] = $size;
+
+                return response()->stream(function () use ($stream) {
+                    fpassthru($stream);
+                }, $response_code, $headers);
+            } else {
+                $filePath = Storage::disk($file->disk)->get($file->path . '/' . $file->hash_name);
+
+                return response($filePath)->withHeaders([
+                    'Content-type' => $file->type,
+                    'Content-Length' => $file->size,
+                    'Accept-Ranges' => 'bytes'
+                ]);
+            }
+
         }
         return abort(404);
     }
